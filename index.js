8160ab6c9c45b47ddb1dbd2eea46c2549b417661
Subhrajit874 <dassubhrajit874@gmail.com>
2025-04-12 23:36:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+ app.get('/twitter', (req,res) =>{
+    res.send('subhdotcom')
+ })
+ app.get('/login', (req,res) =>{
+    res.send('HI EVERYONE')
+ })
+ app.get('/youtube', (req,res) =>{
+    res.send('HIIIIIII')
+ })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
